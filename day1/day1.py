32b326364b865d53f2a38734f388101cd5a54fda
Tom Pollard <tpollard@flashpoint-intel.com>
2021-11-17 15:28:41 -0500

    day 1 solution

--- day1/day1.py
+++ day1/day1.py
@@ -7,13 +7,9 @@ from pathlib import Path
 INPUTFILE = "input.txt"
 
 SAMPLE_INPUT = """
+1969
+100756
 """
-
-SAMPLE_CASES = [
-    (arg1, expected1),
-    (arg2, expected2),
-]
-
 
 # Utility functions
 
@@ -28,56 +24,42 @@ def filter_blank_lines(lines):
     return [line.strip() for line in lines if line.strip()]
 
 
-## Use these if blank lines in input are meaningful.
-def sample_input():
-    return SAMPLE_INPUT.strip("\n").split("\n")
-
-def load_input(infile):
-    return [line.strip() for line in Path(infile).open()]
-
-def parse_sections(lines):
-    result = []
-    sect = []
-    for line in lines:
-        line = line.strip()
-        if not line:
-            if sect:
-                result.append(sect)
-            sect = []
-        else:
-            sect.append(line)
-    if sect:
-        result.append(sect)
-    return result
-
-
 # Solution
+
+def fuel_required(mass):
+    return max((int(mass) // 3) - 2, 0)
+
+def fuel_required2(mass):
+    result = 0
+    fuel = max((int(mass) // 3) - 2, 0)
+    while fuel:
+        result += fuel
+        fuel = max((fuel // 3) - 2, 0)
+    return result
 
 def solve(lines):
     """Solve the problem."""
-    pass
+    total_fuel = 0
+    for line in lines:
+        total_fuel += fuel_required(int(line))
+    return total_fuel
+
+def solve2(lines):
+    """Solve the problem."""
+    total_fuel = 0
+    for line in lines:
+        total_fuel += fuel_required2(int(line))
+    return total_fuel
 
 
 # PART 1
-
-#!! DELETE THE example1 FUNCTION YOU'RE NOT GOING TO USE
-
-def example1():
-    """Run example for problem with input arguments."""
-    print("EXAMPLE 1:")
-    for arg, expected in SAMPLE_CASES:
-        result = solve(arg)
-        print(f"'{arg}' -> {result} (expected {expected})")
-        assert result == expected
-    print("= " * 32)
-
 
 def example1():
     """Run example for problem with input lines."""
     print("EXAMPLE 1:")
     lines = filter_blank_lines(SAMPLE_INPUT.split("\n"))
     result = solve(lines)
-    expected = 0
+    expected = 34237
     print(f"'sample-input' -> {result} (expected {expected})")
     assert result == expected
     print("= " * 32)
@@ -87,11 +69,33 @@ def part1(lines):
     print("PART 1:")
     result = solve(lines)
     print(f"result is {result}")
+    assert result == 3324332
     print("= " * 32)
 
 
 # PART 2
 
+SAMPLE_CASES2 = [
+    (14, 2),
+    (1969, 966),
+    (100756, 50346),
+]
+
+def example2():
+    """Run example for problem with input lines."""
+    print("EXAMPLE 2:")
+    for arg, expected in SAMPLE_CASES2:
+        result = fuel_required2(arg)
+        print(f"'sample-input' -> {result} (expected {expected})")
+        assert result == expected
+    print("= " * 32)
+
+
+def part2(lines):
+    print("PART 2:")
+    result = solve2(lines)
+    print(f"result is {result}")
+    print("= " * 32)
 
 
 if __name__ == "__main__":
