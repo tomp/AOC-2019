0eb85c8a1ae2832114c4f4422ca61d9f2ff38b3e
Tom Pollard <pollard.tom@gmail.com>
2021-11-25 12:02:46 -0500

    day 8 solution

--- day8/day8.py
+++ day8/day8.py
@@ -7,13 +7,12 @@ from pathlib import Path
 INPUTFILE = "input.txt"
 
 SAMPLE_INPUT = """
+123456712012
 """
 
-SAMPLE_CASES = [
-    (arg1, expected1),
-    (arg2, expected2),
-]
-
+SAMPLE_INPUT2 = """
+0222112222120000
+"""
 
 # Utility functions
 
@@ -28,56 +27,58 @@ def filter_blank_lines(lines):
     return [line.strip() for line in lines if line.strip()]
 
 
-## Use these if blank lines in input are meaningful.
-def sample_input():
-    return SAMPLE_INPUT.strip("\n").split("\n")
-
-def load_input(infile):
-    return [line.strip() for line in Path(infile).open()]
-
-def parse_sections(lines):
-    result = []
-    sect = []
-    for line in lines:
-        line = line.strip()
-        if not line:
-            if sect:
-                result.append(sect)
-            sect = []
-        else:
-            sect.append(line)
-    if sect:
-        result.append(sect)
-    return result
-
-
 # Solution
 
-def solve(lines):
+def print_layer(pixels: str, width: int, height: int):
+    """Print the layer."""
+    PIX = {"0": " ", "1": "#"}
+    display = "".join([PIX[v] for v in pixels])
+    print("\n".join([display[v*width:(v+1)*width] for v in range(height)]))
+
+def get_layers(text: str, width: int, height: int) -> list[str]:
+    """Split the input text into layers of size width * height."""
+    npix = len(text.strip())
+    size = width * height
+    assert npix % size == 0
+    return [text[v*size:(v+1)*size] for v in range(npix // size)]
+
+def top_pixel(pixels: list[str]) -> str:
+    for pixel in pixels:
+        if pixel != "2":
+            return pixel
+    return "2"
+
+def solve2(text: str, width: int, height: int) -> str:
     """Solve the problem."""
-    pass
+    layers = get_layers(text, width, height)
+    pixels = "".join([top_pixel(p) for p in zip(*layers)])
+    print_layer(pixels, width, height)
+    return pixels
+
+def solve(text: str, width: int, height: int) -> int:
+    """Solve the problem."""
+    layers = get_layers(text, width, height)
+    size = width * height
+    minZeros = size
+    target = ""
+    for layer in layers:
+        nZeros = layer.count("0")
+        if nZeros < minZeros:
+            minZeros = nZeros
+            target = layer
+    nOnes = target.count("1")
+    nTwos = target.count("2")
+    return nOnes * nTwos
 
 
 # PART 1
-
-#!! DELETE THE example1 FUNCTION YOU'RE NOT GOING TO USE
-
-def example1():
-    """Run example for problem with input arguments."""
-    print("EXAMPLE 1:")
-    for arg, expected in SAMPLE_CASES:
-        result = solve(arg)
-        print(f"'{arg}' -> {result} (expected {expected})")
-        assert result == expected
-    print("= " * 32)
-
 
 def example1():
     """Run example for problem with input lines."""
     print("EXAMPLE 1:")
     lines = filter_blank_lines(SAMPLE_INPUT.split("\n"))
-    result = solve(lines)
-    expected = 0
+    result = solve(lines[0], 3, 2)
+    expected = 1
     print(f"'sample-input' -> {result} (expected {expected})")
     assert result == expected
     print("= " * 32)
@@ -85,13 +86,30 @@ def example1():
 
 def part1(lines):
     print("PART 1:")
-    result = solve(lines)
+    result = solve(lines[0], 25, 6)
     print(f"result is {result}")
+    assert result == 1330
     print("= " * 32)
 
 
 # PART 2
 
+def example2():
+    """Run example for problem with input lines."""
+    print("EXAMPLE 2:")
+    lines = filter_blank_lines(SAMPLE_INPUT2.split("\n"))
+    result = solve2(lines[0], 2, 2)
+    expected = "0110"
+    print(f"'sample-input' -> {result} (expected {expected})")
+    assert result == expected
+    print("= " * 32)
+
+
+def part2(lines):
+    print("PART 1:")
+    result = solve2(lines[0], 25, 6)
+    print(f"result is {result}")
+    print("= " * 32)
 
 
 if __name__ == "__main__":
